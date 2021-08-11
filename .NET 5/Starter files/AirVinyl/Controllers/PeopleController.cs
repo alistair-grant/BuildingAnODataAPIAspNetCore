0b63dac1c5c74e90fe743449993900656ba52771
Alistair Grant <alistair.grant@ptml.com>
2021-08-11 14:47:12 +1200

    Add people controller

---
+++ .NET 5/Starter files/AirVinyl/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using AirVinyl.API.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AirVinyl.Controllers
+{
+    public class PeopleController : ODataController
+    {
+        private readonly AirVinylDbContext _airVinylDbContext;
+
+        public PeopleController(AirVinylDbContext airVinylDbContext)
+        {
+            _airVinylDbContext = airVinylDbContext
+                ?? throw new ArgumentNullException(nameof(airVinylDbContext));
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _airVinylDbContext.People.ToListAsync());
+        }
+    }
+}
