74b12b2045367c0908147fc9aca082027020821c
Alistair Grant <alistair.grant@ptml.com>
2021-08-11 14:47:48 +1200

    Add vinyl records controller

---
+++ .NET 5/Starter files/AirVinyl/Controllers/VinylRecordsController.cs
@@ -0,0 +1,27 @@
+﻿using AirVinyl.API.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AirVinyl.Controllers
+{
+    [Route("odata")]
+    public class VinylRecordsController : ODataController
+    {
+        private readonly AirVinylDbContext _airVinylDbContext;
+
+        public VinylRecordsController(AirVinylDbContext airVinylDbContext)
+        {
+            _airVinylDbContext = airVinylDbContext
+                ?? throw new ArgumentNullException(nameof(airVinylDbContext));
+        }
+
+        [HttpGet("VinylRecords")]
+        public async Task<IActionResult> GetAllVinylRecords()
+        {
+            return Ok(await _airVinylDbContext.VinylRecords.ToListAsync());
+        }
+    }
+}
